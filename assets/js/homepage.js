e424deb0c23dbe42039c113033b0860fc93ed8a0
Drogon <aatrox48@gmail.com>
2022-01-04 21:39:56 -0600

    ability to log any users information to the console

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,4 +10,22 @@ var getUserRepos = function(user){
     });
 };
 
-getUserRepos("facebook");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    };
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
