82a943367c4355cd6bfab1358f96ae3242b0174d
Drogon <aatrox48@gmail.com>
2022-01-04 22:04:00 -0600

    repos now show to the page in container elements

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function(user){
     // format the github api url
     var apiURL = "https://api.github.com/users/" + user + "/repos";
@@ -5,13 +10,10 @@ var getUserRepos = function(user){
     // make a request to the url
     fetch(apiURL).then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
-
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -26,6 +28,35 @@ var formSubmitHandler = function(event){
         alert("Please enter a GitHub username");
     };
     console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for(var i = 0; i < repos.length; i++){
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
